9cec1b1cbecc0f9a0aca0d2a45a254960852c7ad
Nokome Bentley <nokome@stenci.la>
2019-10-08 08:40:47 +1300

    test(Stdio & Tcp): add to and fix tests

--- src/stdio/StdioClientServer.test.ts
+++ src/stdio/StdioClientServer.test.ts
@@ -1,11 +1,20 @@
 import path from 'path'
-import * as stencila from '@stencila/schema'
 import StdioClient from './StdioClient'
 
 describe('StdioClient and StdioServer', () => {
   const client = new StdioClient('npx ts-node ' + path.join(__dirname, 'stdioTestServer.ts'))
 
-  test('make a simple request', async () => {
+  test('decode', async () => {
     expect(await client.decode('3.14')).toEqual(3.14)
+    expect(await client.decode('{"type":"Entity"}', 'json')).toEqual({type: 'Entity'})
+  })
+
+  test('encode', async () => {
+    expect(await client.encode(3.14)).toEqual('3.14')
+    expect(await client.encode({type: 'Entity'}, 'json')).toEqual('{"type":"Entity"}')
+  })
+
+  test('execute', async () => {
+    expect(await client.execute({type: 'Entity'})).toEqual({type: 'Entity'})
   })
 })
--- src/tcp/TcpClientServer.test.ts
+++ src/tcp/TcpClientServer.test.ts
@@ -2,10 +2,12 @@ import TcpClient from './TcpClient'
 import TcpServer from './TcpServer'
 
 const server = new TcpServer()
+let client: TcpClient
 
 beforeAll(async () => {
   server.start()
   await new Promise(resolve => setTimeout(resolve, 1000))
+  client = new TcpClient()
 })
 
 afterAll(() => {
@@ -13,9 +15,18 @@ afterAll(() => {
 })
 
 describe('TcpClient and TcpServer', () => {
-  const client = new TcpClient()
 
-  test('make a simple request', async () => {
-    expect(await client.decode('{}')).toEqual('a string')
+  test('decode', async () => {
+    expect(await client.decode('3.14')).toEqual(3.14)
+    expect(await client.decode('{"type":"Entity"}', 'json')).toEqual({type: 'Entity'})
+  })
+
+  test('encode', async () => {
+    expect(await client.encode(3.14)).toEqual('3.14')
+    expect(await client.encode({type: 'Entity'}, 'json')).toEqual('{"type":"Entity"}')
+  })
+
+  test('execute', async () => {
+    expect(await client.execute({type: 'Entity'})).toEqual({type: 'Entity'})
   })
 })
