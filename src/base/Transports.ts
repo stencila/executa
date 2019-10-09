9949633216131901e554edddd7322c9906992885
Nokome Bentley <nokome@stenci.la>
2019-10-09 18:25:24 +1300

    refactor(serve): Refactor to allow for more than one server

--- src/base/Transports.ts
+++ src/base/Transports.ts
@@ -10,6 +10,7 @@ export interface StdioAddress {
   type: Transport.stdio
   command: string
   args?: string[]
+  cwd?: string
 }
 
 export interface VsockAddress {
@@ -22,6 +23,36 @@ export interface TcpAddress {
   type: Transport.tcp | Transport.http | Transport.ws
   host?: string
   port?: number
+}
+
+export function tcpAddress(
+  address: undefined | string | Omit<TcpAddress, 'type'>,
+  defaults: {
+    host: string
+    port: number
+  }
+): Required<Omit<TcpAddress, 'type'>> {
+  if (address === undefined) {
+    return defaults
+  } else if (typeof address === 'string') {
+    const parts = address.split(':')
+    if (parts.length === 1) {
+      return {
+        host: defaults.host,
+        port: parseInt(parts[0])
+      }
+    } else if (parts.length >= 2) {
+      return {
+        host: parts[0],
+        port: parseInt(parts[1])
+      }
+    } else {
+      return defaults
+    }
+  } else {
+    const { host = defaults.host, port = defaults.port } = address
+    return { host, port }
+  }
 }
 
 export interface HttpAddress extends TcpAddress {
