ae3d8a6249ecea0ebec5d1939d007f5f5d4e7a56
Nokome Bentley <nokome@stenci.la>
2019-10-09 18:46:39 +1300

    fix(JS Manifest): Fix programmingLanguage

--- src/stdio/discover.ts
+++ src/stdio/discover.ts
@@ -55,7 +55,7 @@ const js: Manifest = {
               enum: ['CodeChunk', 'CodeExpression']
             },
             programmingLanguage: {
-              enum: ['python']
+              enum: ['javascript']
             }
           }
         }
