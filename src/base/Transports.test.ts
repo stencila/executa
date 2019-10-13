56464aab3f6c60a4efd12f07758b5c64f698c8fe
Nokome Bentley <nokome@stenci.la>
2019-10-14 10:42:43 +1300

    fix(Transports): Allow for number as port

---
+++ src/base/Transports.test.ts
@@ -0,0 +1,31 @@
+import {tcpAddress} from './Transports'
+
+describe('tcpAddress', () => {
+
+  const defaults = {
+    host: '127.0.0.1',
+    port: 4321
+  }
+
+  test('parses a string with host and port', () => {
+    expect(tcpAddress('example.com:2010', defaults)).toEqual({
+      host: 'example.com',
+      port: 2010
+    })
+  })
+
+  test('parses a number as the port', () => {
+    expect(tcpAddress(2020, defaults)).toEqual({
+      host: '127.0.0.1',
+      port: 2020
+    })
+  })
+
+  test('parses a string with just port', () => {
+    expect(tcpAddress('2030', defaults)).toEqual({
+      host: '127.0.0.1',
+      port: 2030
+    })
+  })
+
+})
