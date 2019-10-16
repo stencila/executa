8c28b84261fb54a452f41a39aeda76e8b4a1ff81
Nokome Bentley <nokome@stenci.la>
2019-10-16 13:28:53 +1300

    feat(VsockFirecrackerClient): Add client to talk to FC microVMs

---
+++ src/vsock/VsockFirecrackerClient.ts
@@ -0,0 +1,35 @@
+import { getLogger } from '@stencila/logga';
+import net from 'net';
+import { VsockAddress } from '../base/Transports';
+import StreamClient from '../stream/StreamClient';
+
+const log = getLogger('executa:vsock:client')
+
+export default class VsockFirecrackerClient extends StreamClient {
+  private socket: net.Socket
+
+  public constructor(address: VsockAddress = new VsockAddress()) {
+    const { path, port} = address
+    if (path === undefined) throw (`Path is required`)
+
+    const socket = net.connect(path)
+    socket.on('connect', () => {
+      log.debug(`${path}:${port}: connecting`)
+      socket.write(`CONNECT ${port}\n`)
+    })
+    socket.on('error', error => {
+      log.error(`${path}:${port}: ${error}`)
+    })
+    socket.on('close', () => {
+      // If there is no server listening in the VM then the connection will be closed.
+      log.debug(`${path}:${port}: connection-closed`)
+    })
+
+    super(socket, socket)
+    this.socket = socket
+  }
+
+  public stop() {
+    this.socket.destroy()
+  }
+}
