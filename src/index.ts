ae2c27e90e3f2bfbfd69ca825045a3f8eb2775f8
Nokome Bentley <nokome@stenci.la>
2019-10-23 17:12:38 +1300

    refactor(Index): Remove usage of default

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
 export { Executor } from './base/Executor'
 
-export { default as stdioDiscover } from './stdio/discover'
-export { default as StdioClient } from './stdio/StdioClient'
-export { default as StdioServer } from './stdio/StdioServer'
+export { discover as stdioDiscover } from './stdio/discover'
+export { StdioClient } from './stdio/StdioClient'
+export { StdioServer } from './stdio/StdioServer'
 
-export {
-  default as VsockFirecrackerClient
-} from './vsock/VsockFirecrackerClient'
-export { default as VsockServer } from './vsock/VsockServer'
+export { VsockFirecrackerClient } from './vsock/VsockFirecrackerClient'
+export { VsockServer } from './vsock/VsockServer'
 
-export { default as TcpClient } from './tcp/TcpClient'
-export { default as TcpServer } from './tcp/TcpServer'
+export { discover as tcpDiscover } from './tcp/discover'
+export { TcpClient } from './tcp/TcpClient'
+export { TcpServer } from './tcp/TcpServer'
 
-export { default as httpDiscover } from './http/discover'
-export { default as HttpClient } from './http/HttpClient'
-export { default as HttpServer } from './http/HttpServer'
+export { discover as httpDiscover } from './http/discover'
+export { HttpClient } from './http/HttpClient'
+export { HttpServer } from './http/HttpServer'
 
-export { default as WebSocketClient } from './ws/WebSocketClient'
-export { default as WebSocketServer } from './ws/WebSocketServer'
+export { WebSocketClient } from './ws/WebSocketClient'
+export { WebSocketServer } from './ws/WebSocketServer'
