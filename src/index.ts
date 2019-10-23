745134fa48e8966a0ea1923d10cd258ad1f69ffa
Nokome Bentley <nokome@stenci.la>
2019-10-24 06:46:31 +1300

    refactor(Index): Export transort address classes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 export { Executor } from './base/Executor'
 
+export { StdioAddress } from './base/Transports'
 export { discover as stdioDiscover } from './stdio/discover'
 export { StdioClient } from './stdio/StdioClient'
 export { StdioServer } from './stdio/StdioServer'
 
+export { VsockAddress } from './base/Transports'
 export { VsockFirecrackerClient } from './vsock/VsockFirecrackerClient'
 export { VsockServer } from './vsock/VsockServer'
 
+export { TcpAddress } from './base/Transports'
 export { discover as tcpDiscover } from './tcp/discover'
 export { TcpClient } from './tcp/TcpClient'
 export { TcpServer } from './tcp/TcpServer'
 
+export { HttpAddress } from './base/Transports'
 export { discover as httpDiscover } from './http/discover'
 export { HttpClient } from './http/HttpClient'
 export { HttpServer } from './http/HttpServer'
 
+export { WebSocketAddress } from './base/Transports'
 export { WebSocketClient } from './ws/WebSocketClient'
 export { WebSocketServer } from './ws/WebSocketServer'
