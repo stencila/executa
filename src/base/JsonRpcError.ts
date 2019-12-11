feb4e67cb0b802c240d2b894fd17821ba0237627
Nokome Bentley <nokome@stenci.la>
2019-12-11 14:30:54 +1300

    docs(JsonRpcError): Fix capitalization

--- src/base/JsonRpcError.ts
+++ src/base/JsonRpcError.ts
@@ -55,19 +55,19 @@ export class JsonRpcError {
   public readonly name = 'JsonRpcError'
 
   /**
-   * A Number that indicates the error type that occurred.
+   * A number that indicates the error type that occurred.
    * This MUST be an integer.
    */
   public readonly code: JsonRpcErrorCode
 
   /**
-   * A String providing a short description of the error.
+   * A string providing a short description of the error.
    * The message SHOULD be limited to a concise single sentence.
    */
   public readonly message: string
 
   /**
-   * A Primitive or Structured value that contains additional information about the error.
+   * A primitive or structured value that contains additional information about the error.
    * This may be omitted.
    * The value of this member is defined by the Server (e.g. detailed error information,
    * nested errors etc.).
