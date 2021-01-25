85999f754b15ff03b094b3c686906b3d04a78b49
alex-ketch <alex@ketch.me>
2021-01-25 13:37:29 -0500

    fix(WS): Fix WebSocket reconnect logic

--- src/ws/WebSocketClient.ts
+++ src/ws/WebSocketClient.ts
@@ -67,6 +67,11 @@ export class WebSocketClient extends Client {
   private stopped = false
 
   /**
+   * Is this client attempting to reestablish a connection
+   */
+  public isRetrying: boolean | undefined
+
+  /**
    * Construct a `WebSocketClient`.
    *
    * @param address The address of the server to connect to
@@ -78,6 +83,41 @@ export class WebSocketClient extends Client {
     super('ws')
     this.address = new WebSocketAddress(address)
     this.options = { ...defaultWebSocketClientOptions, ...options }
+  }
+
+  private retryConnect = (
+    resolve: (a: void | PromiseLike<void>) => void,
+    reject: (a: Error) => void
+  ): Promise<void> => {
+    // Terminate early if already attempting to reconnect
+    if (this.isRetrying === true) {
+      return Promise.resolve()
+    }
+
+    this.isRetrying = true
+    return retry(() => this.start(), {
+      retries: this.options.retries,
+      randomize: true,
+      maxTimeout: 5000,
+      onFailedAttempt: (error) => {
+        if (this.options.logging) {
+          log.info(
+            `Connection attempt ${error.attemptNumber} failed. ${error.retriesLeft} retries left.`
+          )
+        }
+      },
+    })
+      .then(resolve)
+      .catch(() => {
+        reject(
+          new Error(
+            `Failed to reconnect after ${this.options.retries} attempt(s)`
+          )
+        )
+      })
+      .finally(() => {
+        this.isRetrying = false
+      })
   }
 
   /**
@@ -92,34 +132,57 @@ export class WebSocketClient extends Client {
       address,
       options: { retries, logging },
     } = this
-    const socket = (this.socket = new WebSocket(
-      address.url(),
-      generateProtocol(id, address.jwt)
-    ))
-    socket.onmessage = (event: MessageEvent) =>
-      this.receive(event.data.toString())
-    socket.onclose = (event: CloseEvent) => {
-      // Try to reconnect if not explicitly closed or if
-      // authentication failed
-      if (this.stopped === true) return
-      const { code, reason } = event
-      if (code === 4001) {
-        if (logging) log.error(`Failed to authenticate with server: ${reason}`)
-        return
+    return new Promise((resolve, reject) => {
+      this.socket = new WebSocket(
+        address.url(),
+        generateProtocol(id, address.jwt)
+      )
+
+      this.socket.onopen = () => {
+        this.stopped = false
+        resolve()
       }
-      if (retries > 0) {
-        if (logging) log.info(`Connection closed, trying to reconnect`)
-        retry(() => this.start(), {
-          retries,
-          randomize: true,
-        }).catch((error) => log.error(error))
+
+      this.socket.onmessage = (event: MessageEvent) =>
+        this.receive(event.data.toString())
+
+      this.socket.onclose = (event: CloseEvent) => {
+        // Try to reconnect if not explicitly closed or if
+        // authentication failed
+        if (this.stopped === true) return resolve()
+
+        const { code, reason } = event
+        if (code === 4001) {
+          const error = `Failed to authenticate with server: ${reason}`
+          if (logging) {
+            log.error(error)
+          }
+
+          reject(new Error(error))
+        }
+
+        if (retries > 0 && this.isRetrying === undefined) {
+          // Configured to reconnect, but the process hasn't been started yet
+          return this.retryConnect(resolve, reject)
+        } else if (this.isRetrying === true) {
+          // Configured to reconnect, and the process is already in progress
+          return undefined
+        } else {
+          // Not configured to reconnect, or the reconnect process failed
+          reject(new Error('Connection closed.'))
+        }
       }
-    }
-    socket.onerror = (error: ErrorEvent) => {
-      if (logging) log.error(error.message)
-    }
-    this.stopped = false
-    return Promise.resolve()
+
+      this.socket.onerror = (error: ErrorEvent) => {
+        if (logging) log.error(error.message)
+
+        if (retries > 0 && this.isRetrying === undefined) {
+          return this.retryConnect(resolve, reject)
+        } else {
+          reject(new Error(error.message))
+        }
+      }
+    })
   }
 
   /**
